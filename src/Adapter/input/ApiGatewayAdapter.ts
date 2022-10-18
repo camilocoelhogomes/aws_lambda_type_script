0c9ef8a420115c5782619c7ed0a5271288e6555f
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2022-10-18 07:01:10 -0300

    create the main part of the domain and Application part

--- src/Adapter/input/ApiGatewayAdapter.ts
+++ src/Adapter/input/ApiGatewayAdapter.ts
@@ -1,24 +1,18 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult, Context} from 'aws-lambda';
-import {inject, injectable} from 'tsyringe';
-import DemoAppOrquestratorPort from '../../Application/ports/orquestrators/DemoAppOrquestratorPort';
-import {DITokens} from '../../Domain/DITokens';
+import {injectable} from 'tsyringe';
 import {InputPort} from '../inputPort/InputPort';
 
 @injectable()
 export class ApiGatewayAdapter implements InputPort {
-  constructor(
-    @inject(DITokens.DEMO_APP_ORQUESTRATOR_PORT)
-    private useCases: DemoAppOrquestratorPort
-  ) {}
+  constructor() {}
 
   async input(
     event: APIGatewayProxyEvent,
     context: Context
   ): Promise<APIGatewayProxyResult> {
-    const result = await this.useCases.getAll();
     return {
       statusCode: 200,
-      body: JSON.stringify(result),
+      body: '',
     };
   }
 }
