776b0b43d778f4a8fdfae3569737e16c8b1269ff
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2022-10-20 08:54:53 -0300

    prepare the terrain so can build in tsc

--- src/Adapter/input/ApiGatewayAdapter.ts
+++ src/Adapter/input/ApiGatewayAdapter.ts
@@ -1,19 +1,41 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult, Context} from 'aws-lambda';
 import {inject, injectable} from 'tsyringe';
+import {UnregistredTaskFactoriePort} from '../../Application/ports/factories/UnregistredTaskFactoriePort';
 import type TaskAppOrquestrator from '../../Application/ports/orquestrators/DemoAppOrquestratorPort';
 import {DITokens} from '../../Domain/DITokens';
 import {InputPort} from '../inputPort/InputPort';
 
 @injectable()
 export class ApiGatewayAdapter implements InputPort {
+  private readonly router: Record<
+    string,
+    Record<
+      string,
+      (event: APIGatewayProxyEvent) => Promise<APIGatewayProxyResult>
+    >
+  > = {
+    '/task': {
+      GET: this.taskGet.bind(this),
+      POST: this.taskPost.bind(this),
+    },
+  };
+
   constructor(
     @inject(DITokens.TASK_ORQUESTRATOR)
-    private readonly taskOrquestrator: TaskAppOrquestrator
+    private readonly taskOrquestrator: TaskAppOrquestrator,
+    @inject(DITokens.UNREGISTRED_TASK_FACTORIE)
+    private readonly unregistredTaskFactorie: UnregistredTaskFactoriePort
   ) {}
 
   async input(
     event: APIGatewayProxyEvent,
     context: Context
+  ): Promise<APIGatewayProxyResult> {
+    return this.router[event.resource][event.httpMethod](event);
+  }
+
+  private async taskGet(
+    event: APIGatewayProxyEvent
   ): Promise<APIGatewayProxyResult> {
     const now = new Date();
     const now7 = new Date(now);
@@ -21,6 +43,26 @@ export class ApiGatewayAdapter implements InputPort {
     const result = await this.taskOrquestrator.getAllByPeriod(now, now7);
     return {
       statusCode: 200,
+      body: JSON.stringify(result),
+    };
+  }
+
+  private async taskPost(
+    event: APIGatewayProxyEvent
+  ): Promise<APIGatewayProxyResult> {
+    if (!event.body) {
+      throw new Error('sem corpo');
+    }
+    const {name, description, responsable, dueDate} = JSON.parse(event.body);
+    const unregistredTask = this.unregistredTaskFactorie.fromRaw(
+      name,
+      description,
+      responsable,
+      dueDate
+    );
+    const result = await this.taskOrquestrator.register(unregistredTask);
+    return {
+      statusCode: 201,
       body: JSON.stringify(result),
     };
   }
--- src/Domain/DITokens.ts
+++ src/Domain/DITokens.ts
@@ -1,8 +1,8 @@
-export const enum DITokens {
-  UNREGISTRED_TASK_FACTORIE = 'unregistredTaskFactoriePort',
-  RANDOM_PORT = 'randomPort',
-  TASK_REPOSITORIE = 'taskRepositoriePort',
-  TASK_FACTORIE = 'taskFactorie',
-  DYNAMO_CLIENT = 'dynamoClient',
-  TASK_ORQUESTRATOR = 'taskOrquestrator',
-}
+export const DITokens: Record<string, string> = {
+  UNREGISTRED_TASK_FACTORIE: 'unregistredTaskFactoriePort',
+  RANDOM_PORT: 'randomPort',
+  TASK_REPOSITORIE: 'taskRepositoriePort',
+  TASK_FACTORIE: 'taskFactorie',
+  DYNAMO_CLIENT: 'dynamoClient',
+  TASK_ORQUESTRATOR: 'taskOrquestrator',
+};
